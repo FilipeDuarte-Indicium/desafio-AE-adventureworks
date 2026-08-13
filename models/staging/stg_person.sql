3ac2ce48fa6c88d92658ddd0b9fcdae355fd6d1c
filipe.duarte@indicium.ai <filipe.duarte@indicium.ai>
2026-08-13 20:34:16 +0000

    fix: column names

--- models/staging/stg_person.sql
+++ models/staging/stg_person.sql
@@ -3,5 +3,5 @@ with source as (
 )
 select
     cast(_c0 as int) as id_pessoa,
-    cast(concat_ws(' ', _c1, _c2, _c3) as string) as nome_completo
+    cast(trim(concat_ws(' ', _c4, _c6)) as string) as nome_completo
 from source
